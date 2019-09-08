0da9462c45bc5f772880419b11faf85b830d3247
Gary <gengliangyu2008@gmail.com>
2019-09-08 18:19:23 +0800

    extract common codes to a python file

--- classify_neural_network.py
+++ classify_neural_network.py
@@ -1,138 +1,18 @@
-import matplotlib
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import warnings
-import math
-import re
-warnings.filterwarnings('ignore')
-
-#%matplotlib inline
-
-# %%
-# Step 1. Prepare data
-# %%
-from sklearn.preprocessing import label_binarize
-
-# Read original data
-sample_data = pd.read_csv('fifa19_ready_data.csv', encoding='utf-8')
-# Remove ID
-sample_data = sample_data.drop('ID', axis=1)
-# Separate internation rating result with rest
-y = sample_data['International Reputation']
-X = sample_data.drop('International Reputation', axis=1)
-print('X: ', X.shape)
-
-# Binarize the rating result
-# Rating score 5 records is only 6 / 18208, so it cannot be predicated with too little data,
-# we convert the score 5 to 4, so only choose rating scores 1, 2, 3, 4 to classify.
-sample_data['International Reputation'].loc[sample_data['International Reputation'] == 5] = 4
-sample_data.hist(column='International Reputation')
-print(sample_data['International Reputation'].value_counts())
-
-y = label_binarize(y, classes=[1, 2, 3, 4])
-n_classes = y.shape[1]
-print('n_classes: ', n_classes)
-# %%
-# Divide data into training set and testing set
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=1)
-print('X_train: ', X_train.shape, 'X_test: ', X_test.shape)
-
-# print(y_train)
-for i in range(n_classes):
-    print("Rating Score value counts: ", i + 1)
-    print(pd.value_counts(y_train[:, i]))
-    plt.hist(y_train[:, i], bins=3)
-    plt.show()
-    print(" ")
-
-# %%
-# Standardize records
-from sklearn.preprocessing import StandardScaler
-
-scaler = StandardScaler()
-scaler.fit(X_train)
-
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-print('X_train: ', X_train.shape, 'X_test: ', X_test.shape)
-# %%
-# Step 2. Solve with Logistic Regression
-# %%
-from sklearn.metrics import roc_auc_score, roc_curve, auc, make_scorer
-from scipy import interp
-from itertools import cycle
-
-
-# Create function to store ROC computation results for all classification classes
-def compute_roc_auc_in_classes(test_data, test_result, num_classes=n_classes):
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    roc_result = {'fpr': fpr, 'tpr': tpr, 'roc_auc': roc_auc}
-
-    for i in range(num_classes):
-        fpr[i], tpr[i], _ = roc_curve(test_data[:, i], test_result[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(test_data.ravel(), test_result.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(num_classes)]))
-
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-
-    # Finally average it and compute AUC
-    mean_tpr /= n_classes
-
-    # Compute macro-average ROC curve and ROC area
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-    return roc_result
-
-
-# Draw ROC plot based on ROC computation result
-def draw_roc_auc_in_classes(roc_auc_result, learn_method, num_classes=n_classes):
-    fpr = roc_auc_result['fpr']
-    tpr = roc_auc_result['tpr']
-    roc_auc = roc_auc_result['roc_auc']
-
-    # Plot all ROC curves
-    plt.figure()
-    plt.plot(fpr["macro"], tpr["macro"],
-             label='Average ROC (area = {0:0.4f})'.format(roc_auc["macro"]),
-             color='navy', linestyle=':', linewidth=4)
-
-    colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'green'])
-    for i, color in zip(range(num_classes), colors):
-        plt.plot(fpr[i], tpr[i], color=color, lw=1,
-                 label='ROC of rating {0} (area = {1:0.4f})'.format((i + 1), roc_auc[i]))
-
-    plt.plot([0, 1], [0, 1], 'k--', lw=1)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC for {}'.format(learn_method))
-    plt.legend(loc="lower right")
-    plt.show()
-
-
 from sklearn.neural_network import MLPClassifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import GridSearchCV
 
+import warnings
+warnings.filterwarnings('ignore')
+#%matplotlib inline
+
+# Step 1. Prepare data
+from common_functions import *
+
+
 # create model for multi-class and one vs rest mode
 mlp_nn = OneVsRestClassifier(MLPClassifier(early_stopping=True, random_state=1))
-print('Created SVM')
+print('Created MLPClassifier')
 #print("parameters: ", mlp_nn.get_params().keys())
 
 # Create GridSearch to find best model
@@ -163,12 +43,6 @@ hyperparameters = dict(estimator__hidden_layer_sizes=hidden_layer_sizes_param,
 # Fit model using gridsearch
 score_making_func = make_scorer(roc_auc_score, average='macro')
 grid_mlp_nn = GridSearchCV(mlp_nn, hyperparameters, scoring=score_making_func, verbose=0)
-
-
-import time
-def show_time_spent(start_time):
-    time_used = time.time() - start_time
-    return "{}:{}:{}".format(int(time_used / 3600), int(time_used % 3600 / 60), int(time_used % 60))
 
 timer_check = time.time()
 print('start training')
